c71ae11cc580e96336a14f3d0e0e5c85888531e2
onepiecefreak3 <realonepiecefreak@gmx.de>
2021-07-22 00:32:53 +0200

    Add automatic script detection #3;

--- XtractQuery/Program.cs
+++ XtractQuery/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using Komponent.IO;
 using XtractQuery.Interfaces;
 using XtractQuery.Options;
 using XtractQuery.Parsers;
@@ -26,13 +27,15 @@ namespace XtractQuery
                 return;
             }
 
+            #region Check arguments existing
+
             if (!arguments.Operation.Exists)
             {
                 Console.WriteLine("No operation mode was given. Specify an operation mode by using the -o argument.");
                 return;
             }
 
-            if (!arguments.QueryType.Exists)
+            if (arguments.Operation.Values.First() == "c" && !arguments.QueryType.Exists)
             {
                 Console.WriteLine("No query type was given. Specify a query type by using the -t argument.");
                 return;
@@ -44,25 +47,26 @@ namespace XtractQuery
                 return;
             }
 
-            operation = arguments.Operation.Values.First();
-            type = arguments.QueryType.Values.First();
-            file = arguments.QueryFile.Values.First();
+            #endregion
 
+            file = arguments.QueryFile.Values.First();
+            if (!File.Exists(file))
+            {
+                Console.WriteLine($"File '{Path.GetFullPath(file)}' was not found.");
+                return;
+            }
+
+            operation = arguments.Operation.Values.First();
             if (operation != "e" && operation != "c")
             {
                 Console.WriteLine($"The operation mode '{operation}' is not valid. Use -h to see a list of valid operation modes.");
                 return;
             }
 
-            if (type != "xq32" && type != "xseq")
+            type = arguments.QueryType.Values.FirstOrDefault();
+            if (operation == "c" && type != "xq32" && type != "xseq")
             {
                 Console.WriteLine($"The query type '{type}' is not valid. Use -h to see a list of valid query types.");
-                return;
-            }
-
-            if (!File.Exists(file))
-            {
-                Console.WriteLine($"File '{Path.GetFullPath(file)}' was not found.");
                 return;
             }
 
@@ -71,12 +75,31 @@ namespace XtractQuery
             switch (operation)
             {
                 case "e":
+                    type = DetermineType(file);
                     ExtractFile(type, file);
                     break;
 
                 case "c":
                     CreateFile(type, file);
                     break;
+            }
+        }
+
+        private static string DetermineType(string file)
+        {
+            using var fileStream = File.OpenRead(file);
+            using var br = new BinaryReaderX(fileStream);
+
+            switch (br.ReadString(4))
+            {
+                case "XQ32":
+                    return "xq32";
+
+                case "XSEQ":
+                    return "xseq";
+
+                default:
+                    return string.Empty;
             }
         }
 
@@ -142,11 +165,13 @@ namespace XtractQuery
             Console.WriteLine("    Valid operations are: e for extraction, c for creation");
             Console.WriteLine("  -t, --type\t\tThe type of file given");
             Console.WriteLine("    Valid types are: xq32, xseq");
+            Console.WriteLine("    The type is automatically detected when extracting; This argument will not have any effect on operation 'e'");
             Console.WriteLine("  -f, --file\t\tThe file to process");
             Console.WriteLine();
             Console.WriteLine("Example usage:");
-            Console.WriteLine($"\tExtract a xq32 query to human readable text: {Assembly.GetExecutingAssembly().Location} -o e -t xq32 -f [file]");
+            Console.WriteLine($"\tExtract any query to human readable text: {Assembly.GetExecutingAssembly().Location} -o e -f [file]");
             Console.WriteLine($"\tCreate a xq32 query from human readable text: {Assembly.GetExecutingAssembly().Location} -o c -t xq32 -f [file]");
+            Console.WriteLine($"\tCreate a xseq query from human readable text: {Assembly.GetExecutingAssembly().Location} -o c -t xseq -f [file]");
         }
     }
 }
